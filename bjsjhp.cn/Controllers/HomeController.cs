7a5a7fa1f01a69592367aeb8cbb335e64f39f9eb
yunchenglk <yunchenglk@163.com>
2016-03-27 12:52:51 +0800

    1

---
+++ bjsjhp.cn/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bjsjhp.cn.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About(string id)
+        {
+            ViewBag.ID = id;
+            return View();
+        }
+        public ActionResult News(string id)
+        {
+            ViewBag.ID = id;
+            return View();
+        }
+        public ActionResult News_Show(string id)
+        {
+            ViewBag.ID = id;
+            return View();
+        }
+        public ActionResult Message()
+        {
+            return View();
+        }
+        public ActionResult Product(string id)
+        {
+            ViewBag.ID = id;
+            return View();
+        }
+        public ActionResult Produce_(string id)
+        {
+            ViewBag.ID = id;
+            return View();
+        }
+        public ActionResult Product_Show(string id)
+        {
+            ViewBag.ID = id;
+            return View();
+        }
+    }
+}
