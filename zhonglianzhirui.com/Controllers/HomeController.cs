c75387833af6b99239f5ba9a430e1ca30e0bafe4
yunchenglk <yunchenglk@163.com>
2016-05-14 04:25:30 +0800

    zhonglianzhirui

---
+++ zhonglianzhirui.com/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zhonglianzhirui.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Seach()
+        {
+            return View();
+        }
+        public ActionResult Case()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult News()
+        {
+            return View();
+        }
+    }
+}
