9873bed2628fe25047442169d6a51c43d5d4496c
lk <yunchenglk@163.com>
2016-06-09 17:51:42 +0800

    1

--- wytx/Controllers/HomeController.cs
+++ wytx/Controllers/HomeController.cs
@@ -13,5 +13,17 @@ namespace wytx.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Teachers()
+        {
+            return View();
+        }
     }
 }
