3504127f26aa779fa2256ff4dd0d438925734b34
Administrator <Administrator@DESKTOP-619CRSJ>
2016-09-05 01:51:17 +0800

    1

---
+++ longsenkangju.com/Content/js/nav.js
@@ -0,0 +1,23 @@
+﻿$(function() {
+	$("#nav>ul>li").hover(function() {
+		$(this).addClass("sfhover");
+		$("#nav ul ul").slideUp("fast");
+//		$(this).find("a:first").animate({"top": "15px"},"fast");
+//		$(this).find("span").animate({"top": "-35px"},"fast");
+		if (!$(this).find("ul").is(":animated")) $(this).find("ul").slideDown("fast")
+	},
+	function() {
+		$(this).removeClass("sfhover");
+		$(this).find("a:first").animate({"top": "0px"},"fast");
+		$(this).find("span").animate({"top": "0px"},"fast");
+		if (!$(this).find("ul").is(":animated")) $(this).find("ul").slideUp("fast");
+		$("#nav ul ul").slideUp("fast")
+	})
+});
+
+
+$(function(){
+	$(".code1").click(function(){
+		$(".side_left").toggle();
+	})
+})
