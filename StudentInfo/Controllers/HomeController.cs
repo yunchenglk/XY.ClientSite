4d79791ea02e7600bf83ec48daf9e6efe9ec09d1
company <yunchenglk@163.com>
2017-06-24 15:03:29 +0800

    1

--- StudentInfo/Controllers/HomeController.cs
+++ StudentInfo/Controllers/HomeController.cs
@@ -4,34 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace zhonglianzhirui.com.Controllers
+namespace StudentInfo.Controllers
 {
-
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
         public ActionResult About()
         {
+            ViewBag.Message = "Your application description page.";
+
             return View();
         }
-        public ActionResult Seach()
-        {
-            return View();
-        }
-        public ActionResult Case()
-        {
-            return View();
-        }
+
         public ActionResult Contact()
         {
-            return View();
-        }
-        public ActionResult News()
-        {
+            ViewBag.Message = "Your contact page.";
+
             return View();
         }
     }
---
+++ StudentInfo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentInfo.MvcApplication" Language="C#" %>
